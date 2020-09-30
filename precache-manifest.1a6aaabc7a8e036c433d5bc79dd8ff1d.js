f339f62b37957e39e31c152f92263428508729ae
Manda Tran <mtt040@ucsd.edu>
2020-09-29 20:51:09 -0700

    Updates

---
+++ precache-manifest.1a6aaabc7a8e036c433d5bc79dd8ff1d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe5b541488033b26a456f6ff23cca95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "160ee3eae270ab2c9688",
+    "url": "/static/css/main.9df6ee6f.chunk.css"
+  },
+  {
+    "revision": "d46b7315d65581949096",
+    "url": "/static/js/2.8a628a57.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.8a628a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160ee3eae270ab2c9688",
+    "url": "/static/js/main.f6c92e35.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "0d9cd0f69a57b89df154c3c68f19bb63",
+    "url": "/static/media/Wavehaus-95SemiBold.0d9cd0f6.otf"
+  },
+  {
+    "revision": "8d245866122a1e79330a03ad66297cc4",
+    "url": "/static/media/eventPlaceholder.8d245866.jpg"
+  },
+  {
+    "revision": "7c24eca97662f8c25791c6cdfb181061",
+    "url": "/static/media/gbm.7c24eca9.jpg"
+  },
+  {
+    "revision": "99ed2c3de7402550125c5828ffa89794",
+    "url": "/static/media/group.99ed2c3d.jpg"
+  },
+  {
+    "revision": "2cfc8c1427357806cf8284ad848efa81",
+    "url": "/static/media/logo-text.2cfc8c14.svg"
+  }
+]);
