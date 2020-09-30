4597d37e533d4c4b4ac72ca04d48dcffc75a078a
Manda Tran <mtt040@ucsd.edu>
2020-09-29 21:10:28 -0700

    Updates

---
+++ precache-manifest.addd79349e05e3b83ba6ca02af34b2f9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da5ea9e14dd292228928195b0f6a232",
+    "url": "/ucsdcssa/index.html"
+  },
+  {
+    "revision": "e6644236cd7acb0d0ca3",
+    "url": "/ucsdcssa/static/css/main.8adf0801.chunk.css"
+  },
+  {
+    "revision": "1a56553fa337aa4cc2fe",
+    "url": "/ucsdcssa/static/js/2.10f0b392.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/ucsdcssa/static/js/2.10f0b392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6644236cd7acb0d0ca3",
+    "url": "/ucsdcssa/static/js/main.40b7875e.chunk.js"
+  },
+  {
+    "revision": "8f4120bc97360d29ffea",
+    "url": "/ucsdcssa/static/js/runtime-main.5c0e207e.js"
+  },
+  {
+    "revision": "0d9cd0f69a57b89df154c3c68f19bb63",
+    "url": "/ucsdcssa/static/media/Wavehaus-95SemiBold.0d9cd0f6.otf"
+  },
+  {
+    "revision": "8d245866122a1e79330a03ad66297cc4",
+    "url": "/ucsdcssa/static/media/eventPlaceholder.8d245866.jpg"
+  },
+  {
+    "revision": "7c24eca97662f8c25791c6cdfb181061",
+    "url": "/ucsdcssa/static/media/gbm.7c24eca9.jpg"
+  },
+  {
+    "revision": "99ed2c3de7402550125c5828ffa89794",
+    "url": "/ucsdcssa/static/media/group.99ed2c3d.jpg"
+  },
+  {
+    "revision": "2cfc8c1427357806cf8284ad848efa81",
+    "url": "/ucsdcssa/static/media/logo-text.2cfc8c14.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc563f481f268a6cacd48f2facfb0c6a.js"
+  "/ucsdcssa/precache-manifest.addd79349e05e3b83ba6ca02af34b2f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ucsdcssa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
