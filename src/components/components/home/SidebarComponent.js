06931de4eee7a25d14a8dd7de943b077d2dfd188
Manda Tran <mtt040@ucsd.edu>
2020-10-05 02:10:11 -0700

    larger sidebar links

--- src/components/components/home/SidebarComponent.js
+++ src/components/components/home/SidebarComponent.js
@@ -3,8 +3,8 @@ import React from "react";
 class SidebarComponent extends React.Component {
     render() {
         return (
-            <div className="sidebar-component py-2">
-                <p className="sidebar-subtitle">{this.props.item.type}</p>
+            <div className="sidebar-component pb-3">
+                <p className="sidebar-subtitle pb-2">{this.props.item.type}</p>
                 <a className="sidebar-link cta" href={this.props.item.url} alt={this.props.item.url} target="_blank" rel="noopener noreferrer">
                     <span className="sidebar-title">{this.props.item.title}</span>
                     <svg width="13px" height="10px" viewBox="0 0 13 10">
