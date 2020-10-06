47c9210437d44fe8df12ba5e7c17b56554780cde
Manda Tran <mtt040@ucsd.edu>
2020-10-05 20:02:33 -0700

    fix team photo heights, add oct newsletter

--- src/components/data/sidebar/highlightsData.js
+++ src/components/data/sidebar/highlightsData.js
@@ -1,4 +1,10 @@
 const highlightsData = [
+    {   
+        id: 0,
+        title: "October 2020 Newsletter",
+        type: "Newsletter",
+        url: "https://mailchi.mp/e2b243784871/cssa-october-newsletter?e=c1119fda9b"
+    },
     {
         id: 1,
         title: "Fall 2020 GBM",
@@ -10,7 +16,7 @@ const highlightsData = [
         title: "Join Our Discord",
         type: "Announcement",
         url: "https://discord.gg/mEuaWtc"
-    },
+    }
 ]
 
 export default highlightsData;
