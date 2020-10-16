423c29d510dbd7a168aacd04b5c03727db8648d8
Manda Tran <mtt040@ucsd.edu>
2020-10-16 13:18:34 -0700

    add geekgirl event

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -7,6 +7,15 @@ const upcomingEventsData = [
         img: "fall2020/Social1.gif",
         url: "https://fb.me/e/3P36iP5Wl",
         zoom: "http://bit.ly/CSSA_Event"
+    },
+    {
+        id: 2,
+        name: "UX Resume Workshop + Networking Event",
+        description: "",
+        date: "Wed 10/21/2020 | 5PM",
+        img: "fall2020/GeekGirl.png",
+        url: "https://fb.me/e/aVnorNqcm",
+        zoom: "http://bit.ly/CSSA_Event"
     }
 ]
 
