a1cffc8bbbb81e562e4dfed89e9e155b3f41cc56
Vicky <v2chou@ucsd.edu>
2021-09-08 19:48:07 -0700

    Updates

---
+++ precache-manifest.ee518fecaf2c65e0f9d2642f93a2afa0.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525b1579499a165a0a851e9d60a2e5f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e72ac6a27390922a7b29",
+    "url": "/static/css/main.bc0acb6e.chunk.css"
+  },
+  {
+    "revision": "647f69e0107c73465157",
+    "url": "/static/js/2.5362ff1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5362ff1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72ac6a27390922a7b29",
+    "url": "/static/js/main.ddfc4d8a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "9e69be6259817751720ae78d79c39781",
+    "url": "/static/media/Conference.9e69be62.png"
+  },
+  {
+    "revision": "52e0a8cacfb1eec107a31b81751c2ef2",
+    "url": "/static/media/CrossCampus.52e0a8ca.png"
+  },
+  {
+    "revision": "8579b2dd301971da017e82e7f14b18ad",
+    "url": "/static/media/CrossCampusSchedule.8579b2dd.png"
+  },
+  {
+    "revision": "70bc75115640a94d7d30c3e9ad458bfd",
+    "url": "/static/media/DiscordHighlights.70bc7511.png"
+  },
+  {
+    "revision": "4fbb5609e4ee6afa73f1318985e6a166",
+    "url": "/static/media/ExecApps.4fbb5609.png"
+  },
+  {
+    "revision": "ee0d092a17793ddd296bee3d80fb4c1d",
+    "url": "/static/media/GBM1.ee0d092a.png"
+  },
+  {
+    "revision": "01e3820489f2b11bd2ca5556016bf510",
+    "url": "/static/media/GeekGirl.01e38204.png"
+  },
+  {
+    "revision": "fd6407fe9891ed8c8124247175758e28",
+    "url": "/static/media/IntroToDataAnalytics.fd6407fe.png"
+  },
+  {
+    "revision": "5c247b2a5aff8d6c75b63f74a5290a73",
+    "url": "/static/media/LabOpenHouse.5c247b2a.png"
+  },
+  {
+    "revision": "1ed6d67a124019883cfba630a3792239",
+    "url": "/static/media/LabTipsWorkshop.1ed6d67a.png"
+  },
+  {
+    "revision": "6369a5f1f365f7630b216d38020006ea",
+    "url": "/static/media/MockInterviewWorkshop.6369a5f1.png"
+  },
+  {
+    "revision": "97bc910d8c351e6e2f912b3353c44b53",
+    "url": "/static/media/PortfolioReview.97bc910d.png"
+  },
+  {
+    "revision": "181ed4e643d65fcbe261cf0e6c7d64a6",
+    "url": "/static/media/Raffle.181ed4e6.png"
+  },
+  {
+    "revision": "f128a7b46bb6263323cb0ad837634368",
+    "url": "/static/media/Social1.f128a7b4.gif"
+  },
+  {
+    "revision": "5df9bdeec60560d3fa404880469930c7",
+    "url": "/static/media/SummerWorkshop.5df9bdee.png"
+  },
+  {
+    "revision": "0b74a5929189d445227c38bcde5e616d",
+    "url": "/static/media/TalkSeries_DI.0b74a592.png"
+  },
+  {
+    "revision": "450352a371ed9596c44a66651085ddc1",
+    "url": "/static/media/TalkSeries_Lang.450352a3.png"
+  },
+  {
+    "revision": "88e063c9b230f45ba40993b6147eb22a",
+    "url": "/static/media/TalkSeries_ML.88e063c9.png"
+  },
+  {
+    "revision": "64b54f9096ec7e968b22a9221749bca5",
+    "url": "/static/media/TalkSeries_Neuro.64b54f90.png"
+  },
+  {
+    "revision": "0d9cd0f69a57b89df154c3c68f19bb63",
+    "url": "/static/media/Wavehaus-95SemiBold.0d9cd0f6.otf"
+  },
+  {
+    "revision": "7e9f46dc6efff395bbbc5f0f3905ae91",
+    "url": "/static/media/WinterSocial.7e9f46dc.png"
+  },
+  {
+    "revision": "bb5e2b9c5714221d2c702b2eeb99127d",
+    "url": "/static/media/Wordmark_Dark.bb5e2b9c.svg"
+  },
+  {
+    "revision": "4305fb233655a4def9da9f5a5f63c9d1",
+    "url": "/static/media/aditi_formal.4305fb23.jpg"
+  },
+  {
+    "revision": "ef4649a6bced5e0673862b9ee174bcfc",
+    "url": "/static/media/aditi_fun.ef4649a6.jpg"
+  },
+  {
+    "revision": "b60a41962f06c53947104d4a30b06aa0",
+    "url": "/static/media/alex_formal.b60a4196.jpg"
+  },
+  {
+    "revision": "835c86c16e5c65339f4cf1c6b198179a",
+    "url": "/static/media/alex_fun.835c86c1.jpg"
+  },
+  {
+    "revision": "05d773bfb00a5ea2f3178c87d00d0490",
+    "url": "/static/media/anphong_formal.05d773bf.jpg"
+  },
+  {
+    "revision": "2007517a40a49d36c8333e8f60e55406",
+    "url": "/static/media/anphong_fun.2007517a.jpg"
+  },
+  {
+    "revision": "a5369809569edc4e18ad64c495326f6e",
+    "url": "/static/media/blank.a5369809.png"
+  },
+  {
+    "revision": "95bf408d95e792861dd0307f3dd6f839",
+    "url": "/static/media/conference2021.95bf408d.png"
+  },
+  {
+    "revision": "03e8b905745a06d20f7e8c0067f16b55",
+    "url": "/static/media/daniel_formal.03e8b905.jpg"
+  },
+  {
+    "revision": "21f70d53b336181073319e46adff5844",
+    "url": "/static/media/daniel_fun.21f70d53.jpg"
+  },
+  {
+    "revision": "0aee586f30843d1c86242e33602903d9",
+    "url": "/static/media/drake_formal.0aee586f.jpg"
+  },
+  {
+    "revision": "f39479829db95d51006b850930d6bff0",
+    "url": "/static/media/drake_fun.f3947982.jpg"
+  },
+  {
+    "revision": "722e005addce0841674f28ae2c342fe3",
+    "url": "/static/media/emily_formal.722e005a.jpg"
+  },
+  {
+    "revision": "b475d277497702a59ed2cdf647219da3",
+    "url": "/static/media/emily_fun.b475d277.jpg"
+  },
+  {
+    "revision": "8d245866122a1e79330a03ad66297cc4",
+    "url": "/static/media/eventPlaceholder.8d245866.jpg"
+  },
+  {
+    "revision": "ffe9a086ebdd587355f674fdba352df2",
+    "url": "/static/media/federicoRossano.ffe9a086.png"
+  },
+  {
+    "revision": "7c24eca97662f8c25791c6cdfb181061",
+    "url": "/static/media/gbm.7c24eca9.jpg"
+  },
+  {
+    "revision": "99ed2c3de7402550125c5828ffa89794",
+    "url": "/static/media/group.99ed2c3d.jpg"
+  },
+  {
+    "revision": "4e87a1efa34b16481295d43788decc0e",
+    "url": "/static/media/jess_formal.4e87a1ef.jpg"
+  },
+  {
+    "revision": "de942da3b8e31938196f29ca04e08115",
+    "url": "/static/media/jess_fun.de942da3.jpg"
+  },
+  {
+    "revision": "95c05d10d2cc2ce099e64f3ef83589a3",
+    "url": "/static/media/laraRangel.95c05d10.jpg"
+  },
+  {
+    "revision": "6667e7aebe3e6d7649e745071d01d534",
+    "url": "/static/media/luke_formal.6667e7ae.jpg"
+  },
+  {
+    "revision": "9219b5691cc8d0fdd352605182218a92",
+    "url": "/static/media/luke_fun.9219b569.jpg"
+  },
+  {
+    "revision": "e3e6a92b9a55b036ef7ba58da179fd96",
+    "url": "/static/media/manda_formal.e3e6a92b.jpg"
+  },
+  {
+    "revision": "66d9b3fe4f24068205e91eb52873fc18",
+    "url": "/static/media/manda_fun.66d9b3fe.jpg"
+  },
+  {
+    "revision": "c2a9b39df246ceed9a4e003e59b1d5ea",
+    "url": "/static/media/mehail_formal.c2a9b39d.jpg"
+  },
+  {
+    "revision": "a23f0001cf523edbddde251458b52ab4",
+    "url": "/static/media/mehail_fun.a23f0001.jpg"
+  },
+  {
+    "revision": "f1ddc26bbd19f6c72323970822fe27c7",
+    "url": "/static/media/sam_formal.f1ddc26b.jpg"
+  },
+  {
+    "revision": "87445f9f8d3eb085bcf53c71754d78ed",
+    "url": "/static/media/sam_fun.87445f9f.jpg"
+  },
+  {
+    "revision": "83827563440bef8cd8bf8cb813220777",
+    "url": "/static/media/samantha_formal.83827563.jpg"
+  },
+  {
+    "revision": "ce2c2f89955c8dce602c3419eaadaa2e",
+    "url": "/static/media/samantha_fun.ce2c2f89.jpg"
+  },
+  {
+    "revision": "68b7d95d0359493a429f6612cf0d3983",
+    "url": "/static/media/simran_formal.68b7d95d.jpg"
+  },
+  {
+    "revision": "11af3931cb5d1ca18ccfe3108cc6a57f",
+    "url": "/static/media/simran_fun.11af3931.jpg"
+  }
+]);
