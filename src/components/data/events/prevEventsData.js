3785f4419185d89c04cb6caa2f737a536daeb0c0
Vicky <v2chou@ucsd.edu>
2021-09-29 23:43:16 -0700

    updated event page and fixed event images

--- src/components/data/events/prevEventsData.js
+++ src/components/data/events/prevEventsData.js
@@ -1,5 +1,21 @@
 const prevEventsData = [
     {
+        id: 17,
+        name: "Mock Interviews",
+        description: "",
+        date: "Wed, 5/19/2021 | 5pm",
+        img: "spring2021/MockInterviewWorkshop.png",
+        url: "https://fb.me/e/QtSFsbcI",
+    },
+    {
+        id: 16,
+        name: "2021 - 2022 Executive Board Applications",
+        description: "",
+        date: "Apply by Fri, 4/30/2021",
+        img: "spring2021/ExecApps.png",
+        url: "https://www.facebook.com/groups/ucsdbrains/permalink/4214111275279473/",
+    },
+    {
         id: 15,
         name: "Making the Most of This Summer",
         description: "",
--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -1,19 +1,62 @@
 const upcomingEventsData = [
-    {
-        id: 2,
-        name: "Mock Interviews",
-        description: "",
-        date: "Wed, 5/19/2021 | 5pm",
-        img: "spring2021/MockInterviewWorkshop.png",
-        url: "https://fb.me/e/QtSFsbcI",
-    },
+    // {
+    //     id: 8,
+    //     name: "Study Jam",
+    //     date: "Tues, 11/29/2021",
+    //     img: "",
+    //     url: "",
+    // },
+    // {
+    //     id: 7,
+    //     name: "Workshop #2",
+    //     date: "Tues, 11/15/2021",
+    //     img: "",
+    //     url: "",
+    // },
+    // {
+    //     id: 6,
+    //     name: "Lab Open House",
+    //     date: "Tues, 11/8/2021",
+    //     img: "",
+    //     url: "",
+    // },
+    // {
+    //     id: 5,
+    //     name: "Welcome Banquet",
+    //     date: "Tues, 11/1/2021",
+    //     img: "",
+    //     url: "",
+    // },
+    // {
+    //     id: 4,
+    //     name: "Fundraiser #1",
+    //     date: "Wed, 10/19/2021",
+    //     img: "",
+    //     url: "",
+    // },
+    // {
+    //     id: 3,
+    //     name: "Workshop #1",
+    //     date: "Tues, 10/18/2021",
+    //     img: "",
+    //     url: "",
+    // },
+    // {
+    //     id: 2,
+    //     name: "Social #1",
+    //     description: "",
+    //     date: "Tues, 10/11/2021 | 5pm",
+    //     img: "",
+    //     url: "",
+    // },
     {
         id: 1,
-        name: "2021 - 2022 Executive Board Applications",
+        name: "GBM #1",
         description: "",
-        date: "Apply by Fri, 4/30/2021",
-        img: "spring2021/ExecApps.png",
-        url: "https://www.facebook.com/groups/ucsdbrains/permalink/4214111275279473/",
+        date: "Tues, 10/4/2021 | 6-7pm",
+        img: "fall2021/GBM1_IG.png",
+        url: "https://www.facebook.com/events/4611675402217002/?ti=ls",
+        zoom: "https://ucsd.zoom.us/j/92110535384"
     }
 ]
 
