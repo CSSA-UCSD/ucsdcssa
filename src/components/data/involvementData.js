f1dcf64b620b9822fcfd0e7e5bf05a138b5b7082
Vicky <v2chou@ucsd.edu>
2021-10-12 14:31:38 -0700

    added committee app link

--- src/components/data/involvementData.js
+++ src/components/data/involvementData.js
@@ -20,13 +20,15 @@ const involvementData = [
         level: "high",
         name: "Committee Members",
         description: "Be a part of a committee of your choice and assist our executive board members with various responsibilities while getting to know our cognitive science community first hand.",
+        urltitle: "Apply Today",
+        url: "https://docs.google.com/forms/u/1/d/e/1FAIpQLSf-vOeA742Jy-bEvfBsXXeKW0Ls7HjfLT6TfinSx-6wvPyztQ/viewform"
     },
     {
         id: 4,
         level: "high",
         name: "Executive Board Members",
         description: "Become an even more involved part of CSSA, and help organize incredible events working with fellow Cog Sci students and interacting with staff, guest speakers and more! We encourage anyone interested to apply! Applications open TODAY and will close on April 30th, the Friday of Week 5. Find more detailed information on the position duties linked in the Online Application form below.",
-        urltitle: "Apply Today",
+        urltitle: "",
         url: "https://tinyurl.com/cssaboardapply"
     }
 ]
