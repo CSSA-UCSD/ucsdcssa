b2df99a371516536d18e114df5f20428293aaad3
Vicky <v2chou@ucsd.edu>
2022-01-06 13:44:44 -0800

    added winter gbm zoom link

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -4,7 +4,8 @@ const upcomingEventsData = [
         name: "GBM #1",
         date: "Tues, 1/11/2022 | 6-7pm",
         img: "winter2022/Winter_GBM_IG.png",
-        url: "https://www.facebook.com/events/265023225698381/"
+        url: "https://www.facebook.com/events/265023225698381/",
+        zoom: "https://ucsd.zoom.us/j/98070443863"
     },
 ]
 
