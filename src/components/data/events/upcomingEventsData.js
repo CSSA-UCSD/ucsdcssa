bbc089e4a3067a3fead1accf8fb71a11b240f3a2
Vicky <v2chou@ucsd.edu>
2022-01-11 17:48:38 -0800

    updated workshop 3 date

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -11,7 +11,7 @@ const upcomingEventsData = [
         id:2,
         name: "Brainstorming & Ideation Workshop",
         img: "winter2022/Workshop3_IG.png",
-        date: "Thurs, 1/13/2022 | 7-8pm"
+        date: "Wed, 1/19/2022 | 5-6pm"
     }
 ]
 
