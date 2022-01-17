538cd1e7c3749e81f7abd487a75183049855ff40
Vicky <v2chou@ucsd.edu>
2022-01-17 01:56:23 -0800

    moved winter gbm to past events and added slides

--- src/components/data/events/prevEventsData.js
+++ src/components/data/events/prevEventsData.js
@@ -1,5 +1,13 @@
 const prevEventsData = [
     {
+        id: 1,
+        name: "GBM #1",
+        date: "Tues, 1/11/2022 | 6-7pm",
+        img: "winter2022/Winter_GBM_IG.png",
+        url: "https://www.facebook.com/events/265023225698381/",
+        slides: "https://docs.google.com/presentation/d/1VaGGh7B2NcDNXq-OCgyl1sc_GfcrW94TPvYq7aJEhh8/edit?usp=sharing",
+    },
+    {
         id: 10,
         name: "Study Jam",
         date: "Tues, 11/30/2021 | 5-9pm | Geisel 721",
--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -1,12 +1,5 @@
 const upcomingEventsData = [
-    {
-        id: 1,
-        name: "GBM #1",
-        date: "Tues, 1/11/2022 | 6-7pm",
-        img: "winter2022/Winter_GBM_IG.png",
-        url: "https://www.facebook.com/events/265023225698381/",
-        zoom: "https://ucsd.zoom.us/j/98070443863"
-    },
+
     {
         id:2,
         name: "Brainstorming & Ideation Workshop",
