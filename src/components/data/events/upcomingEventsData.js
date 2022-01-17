6e2979de0970899b1c81e9e4a6d68fdbd01a8bad
Vicky <v2chou@ucsd.edu>
2022-01-17 02:03:39 -0800

    added future events

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -10,6 +10,31 @@ const upcomingEventsData = [
         name: "Web Dev Committee",
         img: "winter2022/WebDev_Committee_IG.png",
         date: "DUE Fri, 1/21/2022"
+    },
+    {
+        id: 4,
+        name: "Chipotle Fundraiser",
+        date: "Mon, 1/24/2022",
+    },
+    {
+        id: 5,
+        name: "Talk Series: Machine Learning",
+        date: "Tues, 2/1/2022",
+    },
+    {
+        id: 6,
+        name: "Talk Series: Neuroscience",
+        date: "Wed, 2/9/2022",
+    },
+    {
+        id: 7,
+        name: "Talk Series: Neuroscience",
+        date: "Thurs, 2/17/2022"
+    },
+    {
+        id: 8,
+        name: "Workshop 4: TBA",
+        date: "Thur, 2/24/2022",
     }
 ]
 
