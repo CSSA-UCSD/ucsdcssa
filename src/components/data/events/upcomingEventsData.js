5cac338f3fddff176613497ef7bc01806dde46d6
Vicky <v2chou@ucsd.edu>
2022-02-05 02:44:54 -0800

    added talk series 2

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -9,7 +9,9 @@ const upcomingEventsData = [
     {
         id: 6,
         name: "Talk Series: Neuroscience",
-        date: "Wed, 2/9/2022",
+        date: "Wed, 2/9/2022 | 4pm",
+        img: "winter2022/Talk_Series_2_IG.png",
+        url: "https://fb.me/e/52lQPbWwA"
     },
     {
         id: 7,
