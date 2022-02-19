899a066398a3502491bf7d7072b6c2e14528790f
Vicky <v2chou@ucsd.edu>
2022-02-18 16:30:22 -0800

    moved design talk series to past events

--- src/components/data/events/prevEventsData.js
+++ src/components/data/events/prevEventsData.js
@@ -1,5 +1,12 @@
 const prevEventsData = [
     {
+        id: 7,
+        name: "Talk Series: Design",
+        date: "Thurs, 2/17/2022 | 5pm",
+        img: "winter2022/Talk_Series_3_IG.png",
+        url: "https://fb.me/e/4926oYGd2",
+    },
+    {
         id: 6,
         name: "Talk Series: Neuroscience",
         date: "Wed, 2/9/2022 | 4pm",
--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -1,14 +1,6 @@
 const upcomingEventsData = [
 
-    {
-        id: 7,
-        name: "Talk Series: Design",
-        date: "Thurs, 2/17/2022 | 5pm",
-        img: "winter2022/Talk_Series_3_IG.png",
-        url: "https://fb.me/e/4926oYGd2",
-        zoom: "https://ucsd.zoom.us/j/96805329645"
-    },
-    {
+       {
         id: 8,
         name: "Workshop 4: TBA",
         date: "Thur, 2/24/2022",
