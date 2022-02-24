a0eb1f774f77458158f527f648845c6f7ca00b95
Vicky <v2chou@ucsd.edu>
2022-02-23 20:41:17 -0800

    added workshop 4 img and event link update

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -2,8 +2,11 @@ const upcomingEventsData = [
 
        {
         id: 8,
-        name: "Workshop 4: TBA",
-        date: "Thur, 2/24/2022",
+        name: "Networking Workshop",
+        date: "Thur, 2/24/2022 | 5pm",
+        img: "winter2022/Workshop4_IG.png",
+        zoom: "https://ucsd.zoom.us/j/97092185220",
+        url: ""
     }
 ]
 
