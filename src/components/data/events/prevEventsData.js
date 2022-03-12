31d22608e311765336cbc10e3cdb3c525a206669
Vicky <v2chou@ucsd.edu>
2022-03-11 19:34:23 -0800

    moved past networking workshop to old events

--- src/components/data/events/prevEventsData.js
+++ src/components/data/events/prevEventsData.js
@@ -1,4 +1,12 @@
 const prevEventsData = [
+       {
+        id: 8,
+        name: "Networking Workshop",
+        date: "Thur, 2/24/2022 | 5pm",
+        img: "winter2022/Workshop4_IG.png",
+        zoom: "https://ucsd.zoom.us/j/97092185220",
+        url: ""
+    },
     {
         id: 7,
         name: "Talk Series: Design",
--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -1,13 +1,5 @@
 const upcomingEventsData = [
 
-       {
-        id: 8,
-        name: "Networking Workshop",
-        date: "Thur, 2/24/2022 | 5pm",
-        img: "winter2022/Workshop4_IG.png",
-        zoom: "https://ucsd.zoom.us/j/97092185220",
-        url: ""
-    }
 ]
 
 export default upcomingEventsData;
