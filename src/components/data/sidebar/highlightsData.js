b565fd010c187810f1bafa8b634cc19405e11099
Vicky <v2chou@ucsd.edu>
2022-04-13 02:11:21 -0700

    adjusted highlights bar

--- src/components/data/sidebar/highlightsData.js
+++ src/components/data/sidebar/highlightsData.js
@@ -1,29 +1,36 @@
 const highlightsData = [
     {
+        id: 5,
+        title: "Apply for Exec Board by 5/4!",
+        type: "Executive Board Applications",
+        url: "https://tinyurl.com/cssaboard22-23"
+    }, 
+    {
+        id: 3,
+        title: "Upcoming 2022 Conference",
+        type: "National Cognitive Science Conference",
+        url: "https://cssa-ucsd.github.io/conference2022/",
+    },  
+    {   
+        id: 4,
+        title: "2021 Conference",
+        // type: "2021 National Cognitive Science Conference",
+        url: "http://conference.cssa-ucsd.org/"
+    },
+    {
         id: 1,
         title: "Join Our Newsletter Updates",
         // type: "Involvement",
+        type: "Newsletter",
         url: "https://docs.google.com/forms/d/e/1FAIpQLScIts1c2jST0Ugg90aLlUINk0vKyVjsGCosX81NPVFURvDARQ/viewform",
         //url: "http://eepurl.com/cHcDp1"
     },
-    // {   
-    //     id: 2,
-    //     title: "More Information",
-    //     type: "2021 National Cognitive Science Conference",
-    //     url: "http://conference.cssa-ucsd.org/"
-    // },
-    {
-        id: 2,
-        title: "More Information",
-        type: "2022 National Cognitive Science Conference",
-        url: "https://cssa-ucsd.github.io/conference2022/",
-    },
     {   
-        id: 3,
+        id: 2,
         title: "CSSA Newsletters",
-        type: "Newsletter",
+        // type: "Newsletter",
         url: "https://us15.campaign-archive.com/home/?u=0be7335fcd3adf9f817456791&id=0d419c2303"
-    }    
+    },
 ]
 
 export default highlightsData;
