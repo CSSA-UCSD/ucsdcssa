f63b3f6626d7e934e6f954ee59bbb5c2aed627a9
Vicky <v2chou@ucsd.edu>
2022-04-13 02:20:26 -0700

    removed CSSA merch on sidebar

--- src/components/data/sidebar/supportUsData.js
+++ src/components/data/sidebar/supportUsData.js
@@ -1,9 +1,9 @@
 const supportUsData = [
-    {
-        id: 1,
-        title: "Order CSSA Merchandise Today!",
-        url: "https://docs.google.com/forms/d/e/1FAIpQLScptuvQwVyekk03k39C4QOBM1K_9KR8UhbzImZUcghTZVThtQ/viewform"
-    },
+    // {
+    //     id: 1,
+    //     title: "Order CSSA Merchandise Today!",
+    //     url: "https://docs.google.com/forms/d/e/1FAIpQLScptuvQwVyekk03k39C4QOBM1K_9KR8UhbzImZUcghTZVThtQ/viewform"
+    // },
     {
         id: 2,
         title: "Follow Us on Social Media!",
