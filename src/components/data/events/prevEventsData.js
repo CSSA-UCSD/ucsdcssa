44101985a6eace3843c719e92b81f189dedad6a9
Vicky <v2chou@ucsd.edu>
2022-05-09 15:40:21 -0700

    added workshop 6

--- src/components/data/events/prevEventsData.js
+++ src/components/data/events/prevEventsData.js
@@ -1,5 +1,33 @@
 const prevEventsData = [
     {
+        id: 7,
+        name: "2022-2023 Exec Board Applications",
+        date: "DUE Wed, 5/4/2022",
+        img: "spring2022/Board_Apps_IG.png",
+        url: "https://tinyurl.com/cssaboard22-23",
+    },
+    {
+        id: 4,
+        name: "Conference Day 1",
+        date: "Tues, 4/26/2022 | 5pm",
+        img: "spring2022/4_Day1_Schedule_IG.png",
+        url: "https://fb.me/e/2Dxi4P4ij"
+    },
+    {
+        id: 5,
+        name: "Conference Day 2",
+        date: "Wed, 4/27/2022 | 5pm",
+        img: "spring2022/5_Day2_Schedule_IG.png",
+        url: "https://fb.me/e/2Dxi4P4ij"
+    },
+    {
+        id: 6,
+        name: "Conference Day 3",
+        date: "Thur, 4/28/2022 | 5pm",
+        img: "spring2022/Day3_Schedule_IG.png",
+        url: "https://fb.me/e/2Dxi4P4ij"
+    },
+    {
         id: 2,
         name: "Workshop 5: Cognitive Science Careers",
         date: "Thur, 4/14/2022 | 5pm",
--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -1,38 +1,11 @@
 const upcomingEventsData = [
+
     {
         id: 7,
-        name: "2022-2023 Exec Board Applications",
-        date: "DUE Wed, 5/4/2022",
-        img: "spring2022/Board_Apps_IG.png",
-        url: "https://tinyurl.com/cssaboard22-23",
-    },
-    {
-        id: 4,
-        name: "Conference Day 1",
-        date: "Tues, 4/26/2022 | 5pm",
-        img: "spring2022/4_Day1_Schedule_IG.png",
-        url: "https://fb.me/e/2Dxi4P4ij"
-    },
-    {
-        id: 5,
-        name: "Conference Day 2",
-        date: "Wed, 4/27/2022 | 5pm",
-        img: "spring2022/5_Day2_Schedule_IG.png",
-        url: "https://fb.me/e/2Dxi4P4ij"
-    },
-    {
-        id: 6,
-        name: "Conference Day 3",
-        date: "Thur, 4/28/2022 | 5pm",
-        img: "spring2022/Day3_Schedule_IG.png",
-        url: "https://fb.me/e/2Dxi4P4ij"
-    },
-    {
-        id: 7,
-        name: "Workshop 6: TBA",
+        name: "Workshop 6: Find Your Summer Success",
         date: "Tues, 5/10/2022 | 5pm",
-        img: "",
-        url: ""
+        img: "spring2022/Workshop6_IG.png",
+        url: "https://ucsd.zoom.us/j/97046192320"
     },
     {
         id: 8,
