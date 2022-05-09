6c76d57163607dbf4be33140928b80519119d08b
Vicky <v2chou@ucsd.edu>
2022-05-09 15:45:47 -0700

    updated workshop 6 with event link and zoom

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -1,11 +1,11 @@
 const upcomingEventsData = [
-
     {
         id: 7,
         name: "Workshop 6: Find Your Summer Success",
         date: "Tues, 5/10/2022 | 5pm",
         img: "spring2022/Workshop6_IG.png",
-        url: "https://ucsd.zoom.us/j/97046192320"
+        url: "https://fb.me/e/1no15zeee",
+        zoom: "https://ucsd.zoom.us/j/97046192320"
     },
     {
         id: 8,
