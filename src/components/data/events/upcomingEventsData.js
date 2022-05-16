e669865a108e96eef0e57fc728551e11d6a0144e
Vicky <v2chou@ucsd.edu>
2022-05-16 02:28:35 -0700

    added todd braver img and link

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -4,8 +4,8 @@ const upcomingEventsData = [
         id: 8,
         name: "Todd Braver Round Table",
         date: "Mon, 5/16/2022 | 5pm",
-        img: "",
-        url: ""
+        img: "spring2022/Braver_Roundtable_IG.png",
+        url: "https://fb.me/e/1LS0cx9gc"
     },
 ]
 
