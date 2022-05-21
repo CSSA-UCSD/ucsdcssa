1d7ff6e207482d7a4cc98745f1b3fa751f621615
Vicky <v2chou@ucsd.edu>
2022-05-21 00:38:10 -0700

    added yogurt world link

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -4,7 +4,7 @@ const upcomingEventsData = [
         name: "Yogurt World Fundraiser",
         date: "Mon, 5/23/2022 | 11AM-9PM",
         img: "spring2022/Yogurt_World_Fundraiser_IG.png",
-        url: ""
+        url: "https://fb.me/e/1VTlDrnpQ"
     },
 ]
 
