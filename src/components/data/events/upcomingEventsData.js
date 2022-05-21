770a3abf80698bfc15b3d674f5dc23ca129b21f3
Vicky <v2chou@ucsd.edu>
2022-05-20 18:40:07 -0700

    removed incorrect yogurt world url

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -4,7 +4,7 @@ const upcomingEventsData = [
         name: "Yogurt World Fundraiser",
         date: "Mon, 5/23/2022 | 11AM-9PM",
         img: "spring2022/Yogurt_World_Fundraiser_IG.png",
-        url: "https://fb.me/e/1LS0cx9gc"
+        url: ""
     },
 ]
 
