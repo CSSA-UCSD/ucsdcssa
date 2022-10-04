0dcf65fe0d7a091fac9c074027166f1fda5277b9
Lucy Lennemann <llennema@ucsd.edu>
2022-10-03 23:06:30 -0700

    Updated Workshop 1 event with graphic and event link

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -10,8 +10,8 @@ const upcomingEventsData = [
         id: 2,
         name: "U Can Seriously Design",
         date: "Mon, 10/10/2022 | 5PM-6PM",
-        img: "",
-        url: ""
+        img: "fall2022/Workshop1_IG.png",
+        url: "https://www.facebook.com/events/870919060960068/?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22group_featured_unit%22%2C%22surface%22%3A%22group%22%7D]%2C%22ref_notif_type%22%3Anull%7D"
     }
 ]
 
