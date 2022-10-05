f64a7fb4190b372d70d59cb3e19dee9c6ae1e8b7
Lucy Lennemann <llennema@ucsd.edu>
2022-10-04 23:33:42 -0700

    Added Fall 2022 events to event page

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -12,6 +12,55 @@ const upcomingEventsData = [
         date: "Mon, 10/10/2022 | 5PM-6PM",
         img: "fall2022/Workshop1_IG.png",
         url: "https://www.facebook.com/events/870919060960068/?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22group_featured_unit%22%2C%22surface%22%3A%22group%22%7D]%2C%22ref_notif_type%22%3Anull%7D"
+    },
+    {
+        id: 3,
+        name: "Social 1",
+        date: "10/11/2022",
+        img: "",
+        url: ""
+    },
+    {
+        id: 4,
+        name: "Fundraiser 1",
+        date: "10/19/2022",
+        img: "",
+        url: ""
+    },
+    {
+        id: 5,
+        name: "Lab Open House",
+        date: "11/7/2022",
+        img: "",
+        url: ""
+    },
+    {
+        id: 6,
+        name: "Social 2",
+        date: "11/8/2022",
+        img: "",
+        url: ""
+    },
+    {
+        id: 7,
+        name: "Workshop 2",
+        date: "11/14/2022",
+        img: "",
+        url: ""
+    },
+    {
+        id: 8,
+        name: "Fundraiser 3",
+        date: "11/16/2022",
+        img: "",
+        url: ""
+    },
+    {
+        id: 9,
+        name: "Study Jam",
+        date: "11/29/2022",
+        img: "",
+        url: ""
     }
 ]
 
