d391184af37688616ec787ed4cbf5a557921d712
Lucy Lennemann <llennema@ucsd.edu>
2022-10-07 15:18:55 -0700

    Added Presidential Advisors Luke & Drake

---
+++ src/components/data/team/presAdvisorsData.js
@@ -0,0 +1,20 @@
+const presAdvisorsData = [ 
+    {
+        id: 1,
+        name: "Drake Coy", 
+        position: "Presidential Advisor",
+        img: "exec_2022_2023/drake.jpeg",
+        imgHover: "exec_2022_2023/drake_hover.jpeg",
+        url: "https://www.facebook.com/webraincogsci"
+    },
+    {
+        id: 2,
+        name: "Luke Sztajnkrycer", 
+        position: "Presidential Advisor",
+        img: "exec_2022_2023/luke.jpeg",
+        imgHover: "exec_2022_2023/luke_hover.jpeg",
+        url: "https://www.facebook.com/webraincogsci"
+    }
+]
+
+export default presAdvisorsData;
--- src/components/pages/Team.js
+++ src/components/pages/Team.js
@@ -4,12 +4,14 @@ import Member from "../components/team/Member";
 import execData from "../data/team/execData";
 import presData from "../data/team/presData";
 import advisorsData from "../data/team/advisorsData";
+import presAdvisorsData from "../data/team/presAdvisorsData";
 
 class Team extends React.Component {
     render() {
         const AdvisorsComponent = advisorsData.map(item => < Member key={item.id} item={item}/>);
         const PresComponent = presData.map(item => < Member key={item.id} item={item}/>);
         const ExecComponent = execData.map(item => < Member key={item.id} item={item}/>);
+        const PresAdvisorsComponent = presAdvisorsData.map(item => < Member key={item.id} item={item}/>);
         return (
             <div className="team">
                 <div className="container">
@@ -22,6 +24,9 @@ class Team extends React.Component {
                         </div>
                         <div className="row justify-content-center">
                             {ExecComponent}
+                        </div>
+                        <div className="row justify-content-center">
+                            {PresAdvisorsComponent}
                         </div>
                     </div>
                     <div className="team-advisors">
