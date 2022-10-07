5810e19ceb87d595e12af7fd135969486e27b998
MiquilW <michael.welsome@gmail.com>
2022-10-06 20:59:54 -0700

    Changed team images

--- src/components/data/team/presData.js
+++ src/components/data/team/presData.js
@@ -1,20 +1,20 @@
 const presData = [
     {
         id: 1,
-        name: "Drake Coy", 
-        position: "President",
+        name: "Satomi Ito", 
+        position: "Co-President",
         year: "2023",
-        img: "exec_2021_2022/drake.jpeg",
-        imgHover: "exec_2021_2022/drake_hover.jpeg",
+        img: "exec_2022_2023/satomi.JPG",
+        imgHover: "exec_2022_2023/satomi_hover.JPG",
         url: "https://www.facebook.com/webraincogsci"
     },
     {
         id: 2,
-        name: "Luke Sztajnkrycer",
-        position: "Vice-President",
-        year: "2023",
-        img: "exec_2021_2022/luke.jpeg",
-        imgHover: "exec_2021_2022/luke_hover.jpeg",
+        name: "Anjali Mathi",
+        position: "Co-President",
+        year: "2024",
+        img: "exec_2022_2023/anjali.jpeg",
+        imgHover: "exec_2022_2023/anjali_hover.jpeg",
         url: "https://www.facebook.com/webraincogsci"
     }
 ]
