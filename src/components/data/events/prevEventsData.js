1817be7877ee63bc4985bc9c7f577b8466f25d79
MiquilW <michael.welsome@gmail.com>
2022-10-07 19:27:59 -0700

    Uploaded slides for GBM1

--- src/components/data/events/prevEventsData.js
+++ src/components/data/events/prevEventsData.js
@@ -1,5 +1,13 @@
 const prevEventsData = [
     {
+        id: 11,
+        name: "Fall GBM",
+        date: "Fri, 10/7/2022 | 5PM-6PM",
+        img: "fall2022/GBM1_IG.jpg",
+        url: "https://www.facebook.com/events/780928833183516/?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22group_featured_unit%22%2C%22surface%22%3A%22group%22%7D]%2C%22ref_notif_type%22%3Anull%7D",
+        slides: "https://docs.google.com/presentation/d/14iJPHMS7TV04gffheryqJlKZGIaHPn7K2U270IJPYs8/edit?usp=sharing"
+    },
+    {
         id: 10,
         name: "Yogurt World Fundraiser",
         date: "Mon, 5/23/2022 | 11AM-9PM",
--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -1,12 +1,5 @@
 const upcomingEventsData = [
     {
-        id: 1,
-        name: "Fall GBM",
-        date: "Fri, 10/7/2022 | 5PM-6PM",
-        img: "fall2022/GBM1_IG.jpg",
-        url: "https://www.facebook.com/events/780928833183516/?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22group_featured_unit%22%2C%22surface%22%3A%22group%22%7D]%2C%22ref_notif_type%22%3Anull%7D"
-    },
-    {
         id: 2,
         name: "U Can Seriously Design",
         date: "Mon, 10/10/2022 | 5PM-6PM",
