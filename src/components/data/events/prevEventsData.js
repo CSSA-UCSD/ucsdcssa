767927f7f9261459afa4b80a52528641563a039e
Lucy Lennemann <llennema@ucsd.edu>
2022-10-10 23:58:09 -0700

    Updated events page - trivia & workshop 1

--- src/components/data/events/prevEventsData.js
+++ src/components/data/events/prevEventsData.js
@@ -1,5 +1,13 @@
 const prevEventsData = [
     {
+        id: 12,
+        name: "U Can Seriously Design",
+        date: "Mon, 10/10/2022 | 5PM-6PM",
+        img: "fall2022/Workshop1_IG.png",
+        url: "https://www.facebook.com/events/870919060960068/?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22group_featured_unit%22%2C%22surface%22%3A%22group%22%7D]%2C%22ref_notif_type%22%3Anull%7D",
+        slides: "https://www.canva.com/design/DAFOmerwNNE/QbKCijcM1Hjtc8pczTu2GQ/edit?utm_content=DAFOmerwNNE&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+    },
+    {
         id: 11,
         name: "Fall GBM",
         date: "Fri, 10/7/2022 | 5PM-6PM",
--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -1,16 +1,9 @@
 const upcomingEventsData = [
     {
-        id: 2,
-        name: "U Can Seriously Design",
-        date: "Mon, 10/10/2022 | 5PM-6PM",
-        img: "fall2022/Workshop1_IG.png",
-        url: "https://www.facebook.com/events/870919060960068/?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22group_featured_unit%22%2C%22surface%22%3A%22group%22%7D]%2C%22ref_notif_type%22%3Anull%7D"
-    },
-    {
         id: 3,
-        name: "Social 1",
-        date: "10/11/2022",
-        img: "",
+        name: "Cog Sci Trivia Night",
+        date: "10/18/2022",
+        img: "fall2022/trivia_night.png",
         url: ""
     },
     {
