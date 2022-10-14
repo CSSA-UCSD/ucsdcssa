c531440e6f813bd8950de52a16dcaab6ae5646f0
Vicky <v2chou@ucsd.edu>
2022-10-13 22:28:08 -0700

    updated highlights on home page

--- src/components/data/sidebar/highlightsData.js
+++ src/components/data/sidebar/highlightsData.js
@@ -1,23 +1,10 @@
 const highlightsData = [
     {
-        id: 3,
-        title: "Check out this year's conference!",
-        type: "National Cognitive Science Conference",
-        url: "https://cssa-ucsd.github.io/conference2022/",
-    },  
-    {   
-        id: 4,
-        title: "2021 Conference",
-        // type: "2021 National Cognitive Science Conference",
-        url: "http://conference.cssa-ucsd.org/"
-    },
-    {
         id: 1,
-        title: "Join Our Newsletter Updates",
-        // type: "Involvement",
-        type: "Newsletter",
-        url: "https://docs.google.com/forms/d/e/1FAIpQLScIts1c2jST0Ugg90aLlUINk0vKyVjsGCosX81NPVFURvDARQ/viewform",
-        //url: "http://eepurl.com/cHcDp1"
+        title: "Peer Mentorship App",
+        type: "Involvement",
+        description: "Become a mentor or mentee and foster your connection to the cognitive science community! This is an incredible opportunity for younger students to gain knowledge about careers and classes from a mentor or for older students to gain leadership experience.",
+        url: "https://docs.google.com/forms/d/e/1FAIpQLSfKDk8x3FVaCIpXjg9tkoLP_FbpexiT7GZSFfUNb65VIxk5hw/viewform"
     },
     {   
         id: 2,
@@ -25,6 +12,26 @@ const highlightsData = [
         // type: "Newsletter",
         url: "https://us15.campaign-archive.com/home/?u=0be7335fcd3adf9f817456791&id=0d419c2303"
     },
+    {
+        id: 3,
+        title: "2022 Conference",
+        type: "National Cognitive Science Conference",
+        url: "https://cssa-ucsd.github.io/conference2022/",
+    },  
+    // {   
+    //     id: 4,
+    //     title: "2021 Conference",
+    //     type: "2021 National Cognitive Science Conference",
+    //     url: "http://conference.cssa-ucsd.org/"
+    // },
+    // {
+    //     id: 1,
+    //     title: "Join Our Newsletter Updates",
+    //     // type: "Involvement",
+    //     type: "Newsletter",
+    //     url: "https://docs.google.com/forms/d/e/1FAIpQLScIts1c2jST0Ugg90aLlUINk0vKyVjsGCosX81NPVFURvDARQ/viewform",
+    //     //url: "http://eepurl.com/cHcDp1"
+    // },
 ]
 
 export default highlightsData;
