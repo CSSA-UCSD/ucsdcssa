c2e4c25557c228cd65582487be0dedd33148c8d1
Lucy Lennemann <llennema@ucsd.edu>
2022-11-09 23:37:42 -0800

    Added details for Workshop 2, removed open lab house and social 2

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -1,24 +1,10 @@
 const upcomingEventsData = [
     {
-        id: 5,
-        name: "Lab Open House",
-        date: "11/7/2022",
-        img: "fall2022/LOH_IG.png",
-        url: "https://fb.me/e/39ZZXfIdc"
-    },
-    {
-        id: 6,
-        name: "Social 2",
-        date: "11/8/2022",
-        img: "",
-        url: ""
-    },
-    {
         id: 7,
         name: "Alumni Career Panel",
         date: "11/14/2022",
-        img: "",
-        url: ""
+        img: "fall2022/Workshop2_IG.png",
+        url: "https://www.facebook.com/events/1299202224179430/?acontext=%7B%22event_action_history%22%3A[%7B%22surface%22%3A%22home%22%7D%2C%7B%22mechanism%22%3A%22group_featured_unit%22%2C%22surface%22%3A%22group%22%7D]%2C%22ref_notif_type%22%3Anull%7D"
     },
     {
         id: 8,
