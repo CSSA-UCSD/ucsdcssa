b91f084960cf1e6b8147af80e6de79840d568e2c
Vicky <v2chou@ucsd.edu>
2023-04-11 18:50:36 -0700

    added spring gbm and workshop 1

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -1,6 +1,19 @@
-// date format: "Weekday, MM/DD/YYYY | xPM-xPM"
+// date format: Day, MM/DD/YYYY | xPM-xPM"
 const upcomingEventsData = [
-    
+    {
+        id: 1,
+        url: "https://fb.me/e/2HnIpaH2W",
+        name: "Spring GBM 1",
+        date: "Thurs, 4/13/2023 | 4PM-5PM",
+        img: "spring2023/Spring GBM IG.png"
+    },
+    {
+        id: 2,
+        name: "Round Table Discussion: Ethics of Machine Learning",
+        date: "Mon, 4/17/2023 | 5PM-6PM",
+        img: "spring2023/Workshop 1 IG Post.png",
+        url: "https://fb.me/e/2KA9GxYer",
+    }
 ]
 
 export default upcomingEventsData;
