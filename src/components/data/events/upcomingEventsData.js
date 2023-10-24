571ff434524e4500cd9ac909b5222e2f7a53bce6
Lucy Lennemann <llennema@ucsd.edu>
2023-10-24 11:41:09 -0700

    Added upcoming event

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -1,6 +1,11 @@
 // date format: Day, MM/DD/YYYY | xPM-xPM
 const upcomingEventsData = [
-
+    {
+        id: 1,
+        name: "Transfer Meet & Greet",
+        date: "Fri, 10/27/2023 | 4PM-5PM",
+        img: "fall2023/meetgreetWorkshop.png"
+    }
 ]
 
 export default upcomingEventsData;
