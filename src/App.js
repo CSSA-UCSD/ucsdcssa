2708b6200d62af427176cd52386dd3a07f89e26a
miahalen <mhle@ucsd.edu>
2024-08-13 22:33:57 -0700

    Calendar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+/* global FullCalendar */
+
+import React, { useEffect } from 'react';
 import './style/main.scss';
 
 import Navbar from './components/components/Navbar';
@@ -6,10 +8,23 @@ import Main from './components/Main';
 import Footer from './components/components/Footer';
 
 function App() {
+  useEffect(() => {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      events: [
+        { title: 'Event 1', date: '2024-08-20' },
+        { title: 'Event 2', date: '2024-08-21' }
+      ]
+      
+    });
+    calendar.render();
+  }, []); 
   return (
     <div className="App">
       < Navbar/>
       < Main/>
+      <div id="calendar" className="calendar-container"></div>
       < Footer/>
     </div>
   );
