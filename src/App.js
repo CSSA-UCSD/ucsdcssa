6338e8d9c374cf74c5146af7161c33274be1a601
ttran29 <tot006@ucsd.edu>
2024-10-14 23:48:18 -0700

    whatever idk what this is i forgot honestly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-/* global FullCalendar */
+// /* global FullCalendar */
+
+// import React, { useEffect } from 'react';
+// import './style/main.scss';
+
+// import FullCalendar from '@fullcalendar/react'; // FullCalendar component
+// import dayGridPlugin from '@fullcalendar/daygrid'; // dayGrid plugin
+// import '@fullcalendar/common/main.css'; // Correct path for FullCalendar core styles
+// import '@fullcalendar/daygrid/main.css'; // DayGrid plugin styles
+
+// import Navbar from './components/components/Navbar';
+// import Main from './components/Main';
+// import Footer from './components/components/Footer';
+
+// function App() {
+//   useEffect(() => {
+//     var calendarEl = document.getElementById('calendar');
+//     var calendar = new FullCalendar.Calendar(calendarEl, {
+//       initialView: 'dayGridMonth',
+//       events: [
+//         { title: 'Event 1', date: '2024-08-20' },
+//         { title: 'Event 2', date: '2024-08-21' }
+//       ]
+      
+//     });
+//     calendar.render();
+//   }, []); 
+//   return (
+//     <div className="App">
+//       < Navbar/>
+//       < Main/>
+//       <div id="calendar" className="calendar-container"></div>
+//       < Footer/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 
 import React, { useEffect } from 'react';
 import './style/main.scss';
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+// import '@fullcalendar/daygrid/main.css';
 
 import Navbar from './components/components/Navbar';
 import Main from './components/Main';
@@ -16,16 +58,16 @@ function App() {
         { title: 'Event 1', date: '2024-08-20' },
         { title: 'Event 2', date: '2024-08-21' }
       ]
-      
     });
     calendar.render();
   }, []); 
+
   return (
     <div className="App">
-      < Navbar/>
-      < Main/>
+      <Navbar />
+      <Main />
       <div id="calendar" className="calendar-container"></div>
-      < Footer/>
+      <Footer />
     </div>
   );
 }
--- src/components/components/home/HomeInitial.js
+++ src/components/components/home/HomeInitial.js
@@ -27,7 +27,7 @@ class HomeInitial extends React.Component {
                             </div>
                             <div className="col-md-auto py-4">
                                 <Link to='/involvement' className='cta'>
-                                    <span>TESTGet Involved</span>
+                                    <span>Get Involved</span>
                                     <svg width="13px" height="10px" viewBox="0 0 13 10">
                                         <path d="M1,5 L11,5"></path>
                                         <polyline points="8 1 12 5 8 9"></polyline>
