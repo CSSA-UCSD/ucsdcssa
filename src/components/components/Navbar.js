c1fa20f0b6555e3534545a56e5ae3cb761f94db8
ttran29 <tot006@ucsd.edu>
2024-10-16 17:57:25 -0700

    fixed my error

--- src/components/components/Navbar.js
+++ src/components/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom"; // Change this import
+import { NavLink } from "react-router-dom";
 import Logo from "../../img/logos/Wordmark_Dark.svg";
 
 class Navbar extends React.Component {
@@ -8,20 +8,20 @@ class Navbar extends React.Component {
             <div>
                 <nav className="navbar">
                     <div className="container">
-                        <NavLink to="/" className="navbar-brand"><img src={Logo} alt="CSSA @ UCSD"></img> </NavLink>
+                        <NavLink to="/" exact className="navbar-brand"><img src={Logo} alt="CSSA @ UCSD"></img> </NavLink>
                         <div className="menu">
                             <ul className="nav">
                                 <li className='nav-item'>
-                                    <NavLink to='/' className='nav-link' activeClassName='active'>Home</NavLink>
+                                    <NavLink to='/' exact className='nav-link' activeClassName='active-link'>Home</NavLink>
                                 </li>
                                 <li className='nav-item'>
-                                    <NavLink to='/team' className='nav-link' activeClassName='active'>Team</NavLink>
+                                    <NavLink to='/team' className='nav-link' activeClassName='active-link'>Team</NavLink>
                                 </li>
                                 <li className='nav-item'>
-                                    <NavLink to='/events' className='nav-link' activeClassName='active'>Events</NavLink>
+                                    <NavLink to='/events' className='nav-link' activeClassName='active-link'>Events</NavLink>
                                 </li>
                                 <li className='nav-item'>
-                                    <NavLink to='/involvement' className='nav-link' activeClassName='active'>Involvement</NavLink>
+                                    <NavLink to='/involvement' className='nav-link' activeClassName='active-link'>Involvement</NavLink>
                                 </li>
                                 <li className='nav-item'>
                                     <a href='https://cssa-ucsd.github.io/conference2024/' target='_blank' className='nav-link' rel="noopener noreferrer">Conference</a>
