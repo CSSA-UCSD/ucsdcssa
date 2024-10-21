245a8336c5e8db291c07586085db4e69e22dc9ee
miahalen <mhle@ucsd.edu>
2024-10-21 01:13:54 -0700

    Updating upcoming events

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -2,16 +2,11 @@
 const upcomingEventsData = [
     {
         id: 1,
-        name: "Inside Out 2 Movie Social",
-        date: "Tues, 4/30/2024 | 5PM-7PM",
-        img: "spring2024/InsideOut.png"
+        name: "Fall GBM #1",
+        date: "Thurs, 10/24/2024 | 5PM-6PM",
+        img: "fall2024/FallGBM_1.png"
     },
-    {
-        id: 2,
-        name: "Through the Looking Glass: Annual Cognitive Science Conference",
-        date: "May 11 & May 12",
-        img: "spring2024/conference24.png"
-    },
+
 ]
 
 export default upcomingEventsData;
