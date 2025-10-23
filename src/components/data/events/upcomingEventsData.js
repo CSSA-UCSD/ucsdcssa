ef2f46f3ddf511d7f5dc8fcb3ace1e7a3d57d5e8
ttran29 <tot006@ucsd.edu>
2025-10-22 20:35:18 -0700

    Update events

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -5,6 +5,12 @@ const upcomingEventsData = [
         name: "Haunted Trails Social",
         date: "Saturday, 10/25/2025 | 6:30PM",
         img: "fall2025/haunted.png"
+    },
+    {
+        id: 2,
+        name: "Midterm Mind-Mash",
+        date: "Wednesday, 10/29/2025 | 6PM-8:30PM",
+        img: "fall2025/midterm.png"
     }
 ]
 
