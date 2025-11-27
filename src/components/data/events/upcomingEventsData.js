8178d546e80e9c441bc2007aad70cd51ea5cf271
ttran29 <tot006@ucsd.edu>
2025-11-27 01:26:58 -0800

    Update

--- src/components/data/events/upcomingEventsData.js
+++ src/components/data/events/upcomingEventsData.js
@@ -2,10 +2,21 @@
 const upcomingEventsData = [
     {
         id: 1,
-        name: "Check event calendar!",
-        date: "You are amazing, 11/XX/2025 | XXPM-XXPM",
-        img: "fall2025/noevents.png"
+        name: "Pizookies and PJ's Social",
+        date: "Tuesday, 12/02/2025 | 6PM-8PM",
+        img: "fall2025/pizookie.png"
     }
 ]
 
 export default upcomingEventsData;
+
+// const upcomingEventsData = [
+//     {
+//         id: 1,
+//         name: "Check event calendar!",
+//         date: "You are amazing, 11/XX/2025 | XXPM-XXPM",
+//         img: "fall2025/noevents.png"
+//     }
+// ]
+
+// export default upcomingEventsData;
