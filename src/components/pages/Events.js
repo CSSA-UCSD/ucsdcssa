6060298284f3b277c9fcfef2cfbc54e6375ae8c8
rollan <147675916+rollannguyen@users.noreply.github.com>
2026-03-05 16:14:49 -0800

    update date for mentor mentee study session

--- src/components/pages/Events.js
+++ src/components/pages/Events.js
@@ -74,7 +74,7 @@ const events = [
   { title: "Peer-2-Peer", start: "2026-02-25" },
   { title: "Panda Express Fundraiser", start: "2026-03-04" },
   { title: "Handel's Fundraiser", start: "2026-03-05" },
-  { title: "Mentor Mentee Study Sesh", start: "2026-03-12" },
+  { title: "Mentor Mentee Study Sesh", start: "2026-03-95" },
   // --- SPRING 2026 ---
   { title: "César Chávez Holiday", start: "2026-03-27" },
   { title: "Instruction Begins", start: "2026-03-30" },
